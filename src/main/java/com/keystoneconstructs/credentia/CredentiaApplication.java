3493b5b3ce70c4b5ecc7450694438fe896918538
rishabh-keeshan <rkkeeshan989@gmail.com>
2023-08-27 01:56:28 +0530

    Initial Commit

---
+++ src/main/java/com/keystoneconstructs/credentia/CredentiaApplication.java
@@ -0,0 +1,13 @@
+package com.keystoneconstructs.credentia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentiaApplication.class, args);
+	}
+
+}
